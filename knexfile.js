73f0ac0348947ccb1a20294feab51ca4a8c88168
Paul Mourer <paul.mourer@gmail.com>
2018-12-10 15:25:40 -0700

    added env for remote DB for deployment

--- knexfile.js
+++ knexfile.js
@@ -7,19 +7,8 @@ module.exports = {
     connection: 'postgresql://localhost:5432/snacks_dev'
   },
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 
 };
